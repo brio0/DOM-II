46c3169ee0d580246eb1eac90b012cbc4f990556
Niki Balajadia <niki@Nikis-MacBook-Pro.local>
2022-06-07 10:57:34 -1000

    one done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import './less/index.less'
 
 // Your code goes here!
+const h2 = document.querySelector('.intro h2')
+h2.addEventListener('mouseover', event => {
+    event.target.style.color = 'yellow'
+})
